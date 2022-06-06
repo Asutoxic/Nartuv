19c0892932a1775eeb8047a841bd6ab342660941
Asutoxic <trankiemgiavu.tkgv@gmail.com>
2022-06-06 14:57:37 +0700

    Introduction to Java

---
+++ Module_2/Java/src/Introduction_to_Java/Introduction_to_Java1.java
@@ -0,0 +1,9 @@
+package Introduction_to_Java;
+
+import java.util.Date;
+public class Introduction_to_Java1 {
+    public static void main(String[] args) {
+        Date now = new Date ();
+        System.out.println("Now is: " + now);
+    }
+}
